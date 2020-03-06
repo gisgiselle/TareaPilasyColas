3ada678ac8ba3b5ac1bb3fc696f312271c41ea61
gisgiselle <47361500+gisgiselle@users.noreply.github.com>
2020-03-06 09:39:06 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.scene.control.TextField;
 
 public class Main extends Application {
-
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -23,10 +18,11 @@ public class Main extends Application {
         txtf1.setEditable(false);
         VBox vb3 = new VBox(txtf2);
         txtf2.setEditable(false);
-        Button btn1 = new Button("Boton a Cola");
-        Button btn2 = new Button("Boton a Pila");
-        Button btn3 = new Button("Deque");
-        Button btn4 = new Button("Pop");
+        Button bPick = new Button("Pick");
+        Button bPush = new Button("Push");
+        //enqueue = push
+        Button bDeq = new Button("Dequeue");
+        Button bPop = new Button("Pop");
         TextField txt = new TextField("Inserta un dato");
         VBox vb2 = new VBox(txt,btn1,btn2,btn3,btn4);
         HBox hb = new HBox(vb1,vb2,vb3);
@@ -34,9 +30,22 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+}
+    public void handle(MouseEvent e){
+        Pilas<Integer> pila = new Pilas<>();
+        Colas<Integer> cola = new Colas<>();
+        
 
+    }
 
     public static void main(String[] args) {
         launch(args);
-    }
+               /* Pilas<Integer> pila= new Pilas<>();
+                pila.push(4);
+                pila.push(5);
+                pila.pop();
+               */
+   
+
+            }
 }
