03d23468105d3a96754dedafa864f1a15c5c88f3
ArchanaVermaRodriguez <58054512+ArchanaVermaRodriguez@users.noreply.github.com>
2020-03-10 07:31:28 -0600

    Add files via upload

---
+++ PilasYColasTareaJavaFX/src/ColaSinLista.java
@@ -0,0 +1,60 @@
+public class ColaSinLista<T> {
+
+    private Nodo<T> inicial;
+    //Método que si está vacío el nodo inicial te dice true sino false.
+    public boolean estaVacia(){
+        if(inicial==null)
+            return true;
+        return false;
+    }
+    //Método que le entra un elemento y ese elemento lo coloca en un nodo que lo va a colocar en la última posición
+    public void enqueue(T elemento){
+        Nodo <T> nodo = new Nodo<>(elemento);
+        if(estaVacia())
+            inicial = nodo;
+        else{
+            Nodo <T> temp = inicial;
+            while(temp.getSiguiente()!=null){
+                temp= temp.getSiguiente();
+            }
+            temp.setSiguiente(nodo);
+        }
+    }
+
+    //Método que te regresa el elemento de la última posición sin modificarla
+    public T peek(){
+        Nodo<T> temp = inicial;
+        while(temp.getSiguiente()!=null){
+            temp = temp.getSiguiente();
+        }
+        return temp.getElemento();
+
+
+    }
+
+    //Te regresa el primer elemento que metiste a la Cola e inicial lo cambia por el siguiente.
+    public T dequeue(){
+        if(estaVacia())
+            return null;
+        else{
+            Nodo<T> temp = inicial;
+            inicial = inicial.getSiguiente();
+            return temp.getElemento();
+        }
+    }
+
+
+    public String listaValores(){
+        String resultado = "";
+        Nodo<T> temp= inicial;
+        while(temp!=null){
+            resultado += temp.getElemento()+"\n";
+            temp= temp.getSiguiente();
+        }
+        return resultado;
+    }
+
+
+
+
+}
---
+++ PilasYColasTareaJavaFX/src/Main.java
@@ -0,0 +1,147 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application{
+    public static void Main(String[] args){
+        Application.launch(args);
+
+    }
+
+    public void start (Stage primaryStage) throws Exception{
+        //GridPane donde está contenido todoo
+        GridPane mainGP = new GridPane();
+        mainGP.setPadding( new Insets(10,10, 10, 10));
+        mainGP.setVgap(10);
+        mainGP.setHgap(10);
+
+        //VBox con coordenadas(0,0). Este VBox es donde está la pila y aquí está el textArea donde se va a ver la pila
+        VBox pilaVB = new VBox();
+       pilaVB.setSpacing(10);
+
+        Label pilaLb = new Label("Pila");
+        TextArea pilaTA = new TextArea();
+        pilaTA.setPromptText("La pila está vacía");
+        pilaTA.setEditable(false);
+
+        pilaVB.getChildren().addAll(pilaLb,pilaTA);
+        pilaVB.setAlignment(Pos.CENTER);
+
+        mainGP.setConstraints(pilaVB,0,0);
+
+        //VBox con coordenadas(0,1). Este VBox es donde está los el textfield para ingresar los datos, los botones y también una label para ver el dato que se saca.
+        VBox buttonsVB = new VBox();
+        buttonsVB.setSpacing(15);
+        Label valoresLb = new Label("Ingrese el dato:");
+        TextField valoresTF = new TextField();
+        valoresTF.setPromptText("Ingrese el dato");
+
+        Button btnCola = new Button("Cola");
+
+        Button btnDequeue = new Button("Dequeue");
+
+        Button btnPila = new Button("Pila");
+
+        Button btnPop = new Button("Pop");
+
+        Label sacarLb = new Label("El dato que sacaste es: ");
+        sacarLb.setVisible(false);
+        Label datoLb = new Label();
+
+
+        buttonsVB.getChildren().addAll(valoresLb, valoresTF, btnCola, btnDequeue, btnPila, btnPop, sacarLb,datoLb);
+        buttonsVB.setAlignment(Pos.CENTER);
+
+        mainGP.setConstraints(buttonsVB,1, 0);
+
+        //VBox con coordenadas(0,2). Este VBox es donde está la cola y aquí está el textArea donde se va a ver la cola
+        VBox colaVB = new VBox();
+        colaVB.setSpacing(10);
+
+        Label colaLb = new Label("Cola");
+        TextArea colaTA = new TextArea();
+        colaTA.setPromptText("La cola está vacía");
+        colaTA.setEditable(false);
+
+        colaVB.getChildren().addAll(colaLb, colaTA);
+        colaVB.setAlignment(Pos.CENTER);
+
+        mainGP.setConstraints(pilaVB,2,0);
+
+
+        mainGP.getChildren().addAll(pilaVB,colaVB, buttonsVB);
+
+        ColaSinLista<String> cola= new ColaSinLista<>();
+        PilaSinLista<String> pila = new PilaSinLista<>();
+
+
+        //Acciones de botones
+        btnCola.setOnAction(a-> { if(!valoresTF.getText().isEmpty()){
+                                    cola.enqueue(valoresTF.getText());
+                                    colaTA.setText(cola.listaValores()) ;
+                                    valoresTF.setText("");
+                                     }
+                                sacarLb.setVisible(false);
+                                datoLb.setVisible(false);
+                                });
+
+        btnDequeue.setOnAction(a->{ sacarLb.setVisible(true);
+                                    datoLb.setVisible(true);
+                                    if(colaTA.getText().isEmpty()){
+                                    datoLb.setText(colaTA.getPromptText());
+                                    }
+                                    else{datoLb.setText(cola.dequeue());
+                                    }
+                                    colaTA.setText(cola.listaValores()) ;
+                                  }
+                              );
+
+        btnPila.setOnAction(a-> {
+                    if (!valoresTF.getText().isEmpty()) {
+                        pila.push(valoresTF.getText());
+                        pilaTA.setText(pila.listaValores());
+                        valoresTF.setText("");
+                    }
+                    sacarLb.setVisible(false);
+                    datoLb.setVisible(false);
+                });
+
+        btnPop.setOnAction(a->{sacarLb.setVisible(true);
+                                datoLb.setVisible(true);
+                                if(pilaTA.getText().isEmpty()){
+                                   datoLb.setText(pilaTA.getPromptText());
+                                }
+                                else{ datoLb.setText(pila.pop());
+
+                                }
+
+                               pilaTA.setText(pila.listaValores());
+                                }
+                            );
+
+
+
+
+        Scene scene = new Scene (mainGP);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Colas y Pilas");
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
+
+
+    }
+}
